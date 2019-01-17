6cc3643d4deb2f0410c0dfecea10d609ac310a17
jaeyoon <jaeyoon@bluehol.net>
2019-01-17 16:43:29 +0900

    session 모듈 설치

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var usersRouter = require('./router/users');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(session({
+  secret: 'SOME_SECRET_KEY',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -11,7 +18,12 @@ app.use(bodyParser.json());
 app.use('/users', usersRouter);
 
 app.get('/', function(req, res){
-  res.send('Root');
+  let session = req.session;
+  if (session) {
+    res.send('No session!');
+  } else {
+    res.send('Root');
+  }
 });
 
 app.listen(app.get('port'), function () {
