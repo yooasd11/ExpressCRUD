0d83bcb475e75069350e0e46203ef6a0dae2a774
jaeyoon <jaeyoon@bluehol.net>
2019-01-29 15:57:06 +0900

    로그인 리덕스 처리

--- frontend/src/redux/reducer/auth.reducer.js
+++ frontend/src/redux/reducer/auth.reducer.js
@@ -1,26 +1,42 @@
 import AT from '../actions/ActionTypes';
+import authAction from '../actions/AuthAction';
 import axios from 'axios';
 import { AUTH_URL } from '../../api/config';
+import store from '../store';
 
 const initState = {
 	auth: false,
 	userId: null
 };
-async function fetchAuth() {
-  let response = await axios.get(AUTH_URL, { withCredentials: true });
-  return response.data;
+function fetchAuth() {
+	axios.get(AUTH_URL, { withCredentials: true })
+		.then(res => {
+			store.dispatch({ type: AT.AUTH, payload: res.data });
+		})
+		.catch(err => {
+			if (err.response) {
+				console.log("error response : ", err.response);
+			} else if (err.request) {
+				console.log("error request : ", err.request);
+			} else {
+				console.log("error message : ", err.message);
+			}
+			console.log("error config : ", err.config);
+		});
 }
 
 export default (state = initState, action) => {
 	switch (action.type) {
 		case AT.AUTH:
-      let fetchedAuth = fetchAuth();
-      // TODO : implement middleware for http request
-			return {
-				...state,
-				auth: fetchedAuth.auth,
-				userId: fetchedAuth.userId
-			};
+			if (action.payload) {
+				return {
+					...state,
+					auth: String(action.payload.auth),
+					userId: action.payload.userId
+				};
+			}
+			fetchAuth();
+			return state;
 		default:
 			return state;
 	}
